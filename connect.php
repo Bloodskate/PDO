374b4246b7c18f3890f725f682d6ab842cf76649
Pramesh Bajracharya <pe.messh@gmail.com>
2016-08-21 09:31:10 +0530

    nothing done - one line removed - added or

--- connect.php
+++ connect.php
@@ -3,6 +3,5 @@ try{
 	$handler = new PDO('mysql:host=127.0.0.1;dbname=app','root','');
 	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
-	echo $e->getmessage();
-	die();
+	echo $e->getmessage() or die();
 }
